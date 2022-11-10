f437c5a14119cbdd2b3a8122b83333349d08e579
barry <barryy@dr.com>
2022-11-09 21:42:10 -0800

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,5 @@
+export VPN_SERVER=dcloud-sjc-anyconnect.cisco.com
+### Replace value with User field
+export VPN_USERNAME=        # <---Paste User here!!!
+### Replace value with Password field
+export VPN_PASSWORD=        # <---Paste Password here!!!

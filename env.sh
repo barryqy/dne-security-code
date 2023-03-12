c67a51fe27cac1a2c1a5b6e28e59cb83fa053dac
barry <barryy@dr.com>
2023-03-12 15:40:12 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,14 @@
+########################################################
+### Your Session ID
+export SESSION_ID=          # <---Paste SESSION ID here!!!
+########################################################
+### VPN Credentials
 export VPN_SERVER=dcloud-rtp-anyconnect.cisco.com
 ### Replace value with User field
 export VPN_USERNAME=        # <---Paste User here!!!
 ### Replace value with Password field
 export VPN_PASSWORD=        # <---Paste Password here!!!
+########################################################
+### ISE IP
+export ISE_PUBLIC=          # <---Paste ISE Public IP here!!!
+export ISE_INTERNAL=        # <---Paste ISE Internal IP here!!!

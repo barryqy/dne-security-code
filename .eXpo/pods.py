8d22a514f1b536bc0ca4faa95971270a9c80c2b6
barry <barryy@dr.com>
2024-04-30 15:57:05 -0700

    Update pods.py

--- .eXpo/pods.py
+++ .eXpo/pods.py
@@ -8,6 +8,9 @@ print(f"Expo Name: {expo.name}")
 if api.choose_datacenter():
     # Create an engagement (replace with your email and demo UID)
     engagement = api.create_engagement()
+else:
+  print("Data center not selected, quiting")
+  quit()
 if engagement:
     print(f"Engagement UID: {engagement.uid}")
     # Set lab environment
